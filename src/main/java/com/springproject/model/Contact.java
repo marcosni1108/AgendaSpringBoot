ca871f7f360c6b79219ca690c1782f4017d3a16d
Marcos Silva <marcosbatista10@gmail.com>
2018-08-01 23:12:22 -0300

    Validacao de campos com Hibernate

--- src/main/java/com/springproject/model/Contact.java
+++ src/main/java/com/springproject/model/Contact.java
@@ -3,19 +3,21 @@ package com.springproject.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.Entity;
 import java.util.Date;
 
 @Entity
 @Data
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 public class Contact extends AbstractEntity {
-    private Long id;
+    @NotEmpty
     private String name;
     private String phoneNumber;
+    @NotEmpty
+    @Email
     private String email;
     private String address;
     private String city;
