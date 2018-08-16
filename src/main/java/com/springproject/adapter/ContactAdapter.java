4d25b544850259fce1c83da05dfa4fac95aba724
Marcos Silva <marcosbatista10@gmail.com>
2018-08-16 19:50:25 -0300

    Override do metodo addArgumentResolvers para configurar paginacao dos servicos

--- src/main/java/com/springproject/adapter/ContactAdapter.java
+++ src/main/java/com/springproject/adapter/ContactAdapter.java
@@ -1,9 +1,20 @@
 package com.springproject.adapter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 public class ContactAdapter extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(new PageRequest(0, 5));
+        argumentResolvers.add(resolver);
+    }
 }
