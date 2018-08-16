2d0a2411556eab0ed48384331d811e598258a162
Marcos Silva <marcosbatista10@gmail.com>
2018-08-16 19:49:23 -0300

    Criacao de um adapter para o projeto

---
+++ src/main/java/com/springproject/adapter/ContactAdapter.java
@@ -0,0 +1,9 @@
+package com.springproject.adapter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ContactAdapter extends WebMvcConfigurerAdapter {
+
+}
