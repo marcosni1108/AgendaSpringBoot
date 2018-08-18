4bd07cec4b740d3e27edb9ebfaf5043acd85028e
Marcos Silva <marcosbatista10@gmail.com>
2018-08-18 14:41:38 -0300

    Implementacao do loadUserByUsername para autenticar usuarios persistidos no banco, trazendo as roles

--- src/main/java/com/springproject/service/CustomUserDetailService.java
+++ src/main/java/com/springproject/service/CustomUserDetailService.java
@@ -1,15 +1,32 @@
 package com.springproject.service;
 
+import com.springproject.model.ContatcUser;
+import com.springproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ContatcUser user = Optional.ofNullable(userRepository.findByUsername(username))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(user.getUsername(), user.getPassword(), AuthorityUtils.commaSeparatedStringToAuthorityList(user.getRoles()));
     }
 }
