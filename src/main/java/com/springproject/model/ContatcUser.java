a8a6f49834c12b35d2f475318ae9a007dbf96bd0
Marcos Silva <marcosbatista10@gmail.com>
2018-08-18 13:46:36 -0300

    Criacao do model ContactUser para autenticacao via BD

---
+++ src/main/java/com/springproject/model/ContatcUser.java
@@ -0,0 +1,29 @@
+package com.springproject.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ContatcUser extends AbstractEntity {
+
+    @NotEmpty
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty
+    @JsonIgnore
+    private String password;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private Boolean admin;
+}
