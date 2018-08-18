3c6979dfb994940c16c1e63f62a0e03a1d60cd26
Marcos Silva <marcosbatista10@gmail.com>
2018-08-18 12:38:23 -0300

    Configuracao inicia do spring security para usar inMemoryAuthentication

--- src/main/java/com/springproject/config/SecurityConfig.java
+++ src/main/java/com/springproject/config/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.springproject.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public static final String RULE_USER = "USER";
+    public static final String RULE_ADMIN = "ADMIN";
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("marcos").password("M@rcos8520").roles(RULE_USER, RULE_ADMIN);
+    }
 }
