59a148cfd07bcd86ad2dfc3567426c21659e9d90
Marcos Silva <marcosbatista10@gmail.com>
2018-08-18 12:56:26 -0300

    utilizacao da anotacao @EnableGlobalMethodSecurity para testar a autenticacao de metodos que mudam o estado dos dados

--- src/main/java/com/springproject/config/SecurityConfig.java
+++ src/main/java/com/springproject/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.springproject.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public static final String RULE_USER = "USER";
