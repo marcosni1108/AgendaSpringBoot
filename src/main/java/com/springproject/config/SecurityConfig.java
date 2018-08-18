567ac3603490d8a98021b4c505faf2ef9d23c40a
Marcos Silva <marcosbatista10@gmail.com>
2018-08-18 12:57:02 -0300

    desabilitanto o csrf

--- src/main/java/com/springproject/config/SecurityConfig.java
+++ src/main/java/com/springproject/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
     }
 
     @Autowired
